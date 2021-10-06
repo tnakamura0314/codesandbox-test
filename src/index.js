6c87dd0dbc3a7592af87df10381f6216540760a5
tnakamura0314 <70365627+tnakamura0314@users.noreply.github.com>
2021-10-06 13:43:22 +0900

    テンプレート文字列について

--- src/index.js
+++ src/index.js
@@ -58,6 +58,21 @@ val5.push("monkey");
 console.log(val5);
 */
 
+/**　テンプレート文字列　*/
+
+const name = "じゃけぇ";
+const age = 25;
+
+//「私の名前はじゃけぇです。年齢は25歳です。」
+
+//従来の方法
+const message1 = "私の名前は" + name + "です。年齢は" + age + "です。";
+console.log(message1);
+
+//テンプレート文字列を用いた方法
+const message2 = `私の名前は${name}です。年齢は${age}です。`;
+console.log(message2);
+
 document.getElementById("app").innerHTML = `
 <h1>Hello Vanilla!</h1>
 <div>
